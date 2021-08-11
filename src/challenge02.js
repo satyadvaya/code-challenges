9890e7c433a2cfe4568fee5d0f63e5c11732d26b
satyadvaya <satyadvaya@gmail.com>
2021-08-11 13:43:54 -0700

    working CC02 fiveCharactersOrFewerOnly

--- src/challenge02.js
+++ src/challenge02.js
@@ -5,3 +5,8 @@ export function fiveAndGreaterOnly(arr) {
 export function evensOnly(arr) {
     return arr.filter((number) => number % 2 === 0);
 }
+
+export function fiveCharactersOrFewerOnly(arr) {
+    const newArr = arr.filter((a) => a.length <= 5);
+    return newArr.sort((a, b) => a.length - b.length);
+}
--- src/challenge02.test.js
+++ src/challenge02.test.js
@@ -1,4 +1,4 @@
-import { fiveAndGreaterOnly, evensOnly } from './challenge02.js';
+import { fiveAndGreaterOnly, evensOnly, fiveCharactersOrFewerOnly } from './challenge02.js';
 
 test('five and greater only', () => {
   const input = [3, 6, 8, 2]; // arrange
@@ -10,4 +10,10 @@ test('given an array of numbers, returns a new array that only includes the even
   const input = [3, 6, 8, 2]; // arrange
   const output = evensOnly(input); // act
   expect(output).toEqual([6, 8, 2]); // assert
+});
+
+test('given an array of strings, returns a new array that only includes those that are 5 characters or fewer in length', () => {
+  const input = ['dog', 'wolf', 'by', 'family', 'eaten', 'camping']; // arrange
+  const output = fiveCharactersOrFewerOnly(input); // act
+  expect(output).toEqual(['by', 'dog', 'wolf', 'eaten']); // assert
 });
