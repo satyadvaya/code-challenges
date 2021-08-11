982d6dc599e6032276fd1d0d42b7850f788f2ebe
satyadvaya <satyadvaya@gmail.com>
2021-08-11 13:34:10 -0700

    working CC02 evensOnly

--- src/challenge02.js
+++ src/challenge02.js
@@ -1,3 +1,7 @@
 export function fiveAndGreaterOnly(arr) {
     return arr.filter((number) => number >= 5);
 }
+
+export function evensOnly(arr) {
+    return arr.filter((number) => number % 2 === 0);
+}
--- src/challenge02.test.js
+++ src/challenge02.test.js
@@ -1,7 +1,13 @@
-import { fiveAndGreaterOnly } from './challenge02.js';
+import { fiveAndGreaterOnly, evensOnly } from './challenge02.js';
 
 test('five and greater only', () => {
   const input = [3, 6, 8, 2]; // arrange
   const output = fiveAndGreaterOnly(input); // act
   expect(output).toEqual([6, 8]); // assert
+});
+
+test('given an array of numbers, returns a new array that only includes the even numbers', () => {
+  const input = [3, 6, 8, 2]; // arrange
+  const output = evensOnly(input); // act
+  expect(output).toEqual([6, 8, 2]); // assert
 });
