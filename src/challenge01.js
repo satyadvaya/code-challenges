2e1701f729538d3c33e163a348da7e74b226ea1b
satyadvaya <satyadvaya@gmail.com>
2021-08-10 21:33:30 -0700

    working CC01 capitalizeNames and namesOnly

--- src/challenge01.js
+++ src/challenge01.js
@@ -1,7 +1,23 @@
 export function doubleNumbers(arr) {
-    return arr.map((num) => num * 2);
+    return arr.map((number) => number * 2);
 }
 
 export function stringItUp(arr) {
-    return arr.map((num) => num.toString());
+    return arr.map((number) => number.toString());
+}
+
+export function capitalizeNames(arr) {
+    return arr.map((name) => name[0].toUpperCase() + name.slice(1).toLowerCase());
+}
+
+export function namesOnly(arr) {
+    return arr.map((person) => person.name);
+}
+
+export function makeStrings(arr) {
+
+}
+
+export function readyToPutInTheDOM(arr) {
+
 }
--- src/challenge01.test.js
+++ src/challenge01.test.js
@@ -1,16 +1,73 @@
-import { doubleNumbers, stringItUp } from './challenge01.js';
+import { doubleNumbers, stringItUp, capitalizeNames, namesOnly, makeStrings, readyToPutInTheDOM } from './challenge01.js';
 
-test('doubles numbers', () => {
+test('maps to array of doubled numbers', () => {
   const input = [2, 5, 100]; // arrange
   const output = doubleNumbers(input); // act
   expect(output).toEqual([4, 10, 200]); // assert
 });
 
-// next test for capitalizeNames (add import too)
-
-
-test('string it up', () => {
+test('takes an array of numbers and make them strings', () => {
   const input = [2, 5, 100]; // arrange
   const output = stringItUp(input); // act
   expect(output).toEqual(["2", "5", "100"]); // assert
+});
+
+test('capitalizes each of an array of names', () => {
+  const input = ["john", "JACOB", "jinGleHeimer", "schmidt"]; // arrange
+  const output = capitalizeNames(input); // act
+  expect(output).toEqual(["John", "Jacob", "Jingleheimer", "Schmidt"]); // assert
+});
+
+test('makes an array of strings of the names', () => {
+  const input = [
+    { name: "Angelina Jolie", age: 80 },
+    { name: "Eric Jones", age: 2 },
+    { name: "Paris Hilton", age: 5 },
+    { name: "Kayne West", age: 16 },
+    { name: "Bob Ziroll", age: 100 }
+  ]; // arrange
+  const output = namesOnly(input); // act
+  expect(output).toEqual([
+    "Angelina Jolie", 
+    "Eric Jones", 
+    "Paris Hilton", 
+    "Kayne West", 
+    "Bob Ziroll"
+  ]); // assert
+});
+
+test('makes an array of strings of the names saying whether or not they can go to The Matrix (18 or older)', () => {
+  const input = [
+    { name: "Angelina Jolie", age: 80 },
+    { name: "Eric Jones", age: 2 },
+    { name: "Paris Hilton", age: 5 },
+    { name: "Kayne West", age: 16 },
+    { name: "Bob Ziroll", age: 100 }
+  ]; // arrange
+  const output = makeStrings(input); // act
+  expect(output).toEqual([
+    "Angelina Jolie can go to The Matrix", 
+    "Eric Jones is under age!!", 
+    "Paris Hilton is under age!!", 
+    "Kayne West is under age!!", 
+    "Bob Ziroll can go to The Matrix"
+  ]); // assert
+});
+
+test('makes an array of the names in h1s, and the ages in h2s', () => {
+  const input = [
+    { name: "Angelina Jolie", age: 80 },
+    { name: "Eric Jones", age: 2 },
+    { name: "Paris Hilton", age: 5 },
+    { name: "Kayne West", age: 16 },
+    { name: "Bob Ziroll", age: 100 }
+  ]; // arrange
+  const output = readyToPutInTheDOM(input); // act
+  expect(output).toEqual([
+    "<h1>Angelina Jolie</h1><h2>80</h2>", 
+    "<h1>Eric Jones</h1><h2>2</h2>", 
+    "<h1>Paris Hilton</h1><h2>5</h2>", 
+    "<h1>Kayne West</h1><h2>16</h2>", 
+    "<h1>Bob Ziroll</h1><h2>100</h2>"
+  ]); // assert
 });
