660b8064e67d07154eb215de144f65a2fbf5a019
satyadvaya <satyadvaya@gmail.com>
2021-08-16 23:45:25 -0700

    working CC03 getFilteredKey

--- src/challenge03/challenge03.js
+++ src/challenge03/challenge03.js
@@ -7,3 +7,7 @@ export function capitalizeObjectKeys(obj) {
 export function sortedKeys(obj) {
     return Object.keys(obj).sort((a, b) => a.length - b.length);
 }
+
+export function getFilteredKey(obj) {
+    return Object.keys(obj).filter((key) => key === 'age');
+}
--- src/challenge03/challenge03.test.js
+++ src/challenge03/challenge03.test.js
@@ -1,4 +1,4 @@
-import { capitalizeObjectKeys, sortedKeys } from './challenge03.js';
+import { capitalizeObjectKeys, sortedKeys, getFilteredKey } from './challenge03.js';
 
 // Object.keys()
 
@@ -12,4 +12,10 @@ test('given an object, it returns an array that has the keys sorted smallest to 
   const dataObject = { name: 'Spot', age: 3, isDog: true, friends: ['Rover', 'Steve', 'Fluffy'] }; //arrange
   const output = sortedKeys(dataObject); //act
   expect(output).toEqual(['age', 'name', 'isDog', 'friends']); //assert
+});
+
+test('given an object, it returns a new array that only includes the object key age', () => {
+  const dataObject = { name: 'Angelina Jolie', isSpot: false, age: 80, }; //arrange
+  const output = getFilteredKey(dataObject); //act
+  expect(output).toEqual(['age']); //assert
 });
